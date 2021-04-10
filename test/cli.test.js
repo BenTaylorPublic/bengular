2347d1ff760867f378bcd792b66b994bb441c2f1
Ben <benjaminjacktaylor@hotmail.com>
2021-04-10 11:45:05 +1000

    Changed cli to a function

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,10 +1,28 @@
 const chai = require("chai");
 const expect = chai.expect;
 
-describe("Cli", function () {
-    describe("Version", function () {
-        it("Should be a string", function () {
-            //Not done
+describe("cli", function () {
+    let cli;
+    before(function () {
+        cli = require("../lib/cli");
+    });
+    describe("version", function () {
+        beforeEach(function () {
+            process.argv.push("version");
         });
+        it("Should be a string", function () {
+            console.log = function (versionMessage) {
+                expect(versionMessage).to.be.a("string");
+                expect(versionMessage).to.include("Bengular v");
+            };
+            cli();
+        })
+        it("Should match package json", function () {
+            const bengularPackageJson = require("../package.json");
+            console.log = function (versionMessage) {
+                expect(versionMessage).to.include(bengularPackageJson.version);
+            };
+            cli();
+        })
     });
 });
