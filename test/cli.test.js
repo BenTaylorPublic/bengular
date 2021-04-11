aab4b11c0156c228454938681226a4d59c65fcbf
Ben <benjaminjacktaylor@hotmail.com>
2021-04-11 11:49:10 +1000

    Added tests for help

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,13 +1,41 @@
 const chai = require("chai");
 const expect = chai.expect;
 
+function removeArgs(args) {
+    for (let i = args.length; i >= 0; i--) {
+        if (["help", "version", "init", "dev", "build"].includes(args[i])) {
+            args.splice(i, 1);
+        }
+    }
+}
+
 describe("cli", function () {
     let cli;
     before(function () {
         cli = require("../lib/cli");
     });
+    describe("help", function () {
+        beforeEach(function () {
+            removeArgs(process.argv);
+            process.argv.push("help");
+        });
+        it("Should output multiple help options", function () {
+            let result = [];
+            console.log = function (helpMessage) {
+                result.push(helpMessage);
+            };
+            cli.cli();
+            expect(result).to.be.a("array");
+            expect(result).to.have.length.above(1);
+            for (let i = 0; i < result.length; i++) {
+                expect(result[i]).to.be.a("string");
+            }
+            expect(result).to.include("Usage: bengular <command>\n");
+        });
+    });
     describe("version", function () {
         beforeEach(function () {
+            removeArgs(process.argv);
             process.argv.push("version");
         });
         it("Should be a string", function () {
@@ -18,7 +46,7 @@ describe("cli", function () {
             cli.cli();
             expect(result).to.be.a("string");
             expect(result).to.include("Bengular v");
-        })
+        });
         it("Should match package json", function () {
             const bengularPackageJson = require("../package.json");
             let result;
@@ -27,6 +55,6 @@ describe("cli", function () {
             };
             cli.cli();
             expect(result).to.include(bengularPackageJson.version);
-        })
+        });
     });
 });
