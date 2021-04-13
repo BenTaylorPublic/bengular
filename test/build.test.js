353213a11313066c3b15dccb7f4d642a67186267
Ben <benjaminjacktaylor@hotmail.com>
2021-04-13 21:34:23 +1000

    Finished build tests

--- test/build.test.js
+++ test/build.test.js
@@ -3,7 +3,7 @@ const expect = chai.expect;
 const {spawnSync} = require("child_process");
 
 function startBengularSync(options) {
-    return spawn(
+    return spawnSync(
         "node",
         ["../../../bin/bengular",
             ...options
@@ -24,35 +24,47 @@ describe("📡 build", function () {
     });
 
     it("📡 Should dev build test project", function () {
+        this.timeout(10_000);
         let bengularBuildResult = startBengularSync(["build", "--dev"]);
 
         //Check the result
-        console.info(bengularBuildResult.stdout);
-        //Ensure the generated/build/ directory in temp contains:
-        //index.html
-        //index.css
-        //index.bundle.js
-        //shared.bundle.js
-        //global-styles.scss
+        expect(bengularBuildResult.stdout).to.contain("Build type: dev");
+        expect(bengularBuildResult.stdout).to.contain("Built in ");
 
-        //Remove the skip once it's all working
-        this.skip();
+        const fs = require("fs");
+        //Ensure the generated/build/ directory in temp contains:
+        expect(fs.existsSync("./generated/build/index.html")).to.be.true;
+        expect(fs.existsSync("./generated/build/index.css")).to.be.true;
+        expect(fs.existsSync("./generated/build/index.bundle.js")).to.be.true;
+        expect(fs.existsSync("./generated/build/shared.bundle.js")).to.be.true;
+        expect(fs.existsSync("./generated/build/global-styles.css")).to.be.true;
+
+        //Ensure that the index.html in generated/build/ still has a component reference in it
+        const indexHtmlInDocs = fs.readFileSync("./generated/build/index.html", "utf8");
+        expect(indexHtmlInDocs).to.contain("<component data-name=\"example-component\"></component>");
+        expect(indexHtmlInDocs).to.not.contain("This is an example component");
     });
 
     it("📡 Should prod build test project", function () {
+        this.timeout(10_000);
         let bengularBuildResult = startBengularSync(["build", "--prod"]);
 
         //Check the result
-        console.info(bengularBuildResult.stdout);
-        //Ensure the docs directory in temp contains:
-        //index.html
-        //index.css
-        //index.bundle.js
-        //shared.bundle.js
-        //global-styles.scss
+        expect(bengularBuildResult.stdout).to.contain("Build type: prod");
+        expect(bengularBuildResult.stdout).to.contain("Built in ");
 
-        //Remove the skip once it's all working
-        this.skip();
+        const fs = require("fs");
+        //Ensure the docs directory in temp contains:
+        expect(fs.existsSync("./docs/index.html")).to.be.true;
+        expect(fs.existsSync("./docs/index.css")).to.be.true;
+        expect(fs.existsSync("./docs/index.bundle.js")).to.be.true;
+        expect(fs.existsSync("./docs/shared.bundle.js")).to.be.true;
+        expect(fs.existsSync("./docs/global-styles.css")).to.be.true;
+
+        //Ensure that the index.html in docs doesn't still have a component reference in it
+        const indexHtmlInDocs = fs.readFileSync("./docs/index.html", "utf8");
+        expect(indexHtmlInDocs).to.contain("This is an example component");
+        expect(indexHtmlInDocs).to.not.contain("<component data-name=\"example-component\"></component>");
     });
 
     after(function () {
